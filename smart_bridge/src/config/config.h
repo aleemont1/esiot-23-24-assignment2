ec6d00570643c74847f0214878ca98ae619a9fa5
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-11-23 22:11:48 +0100

    Introduces N, MAX and MIN temporary values

--- smart_bridge/src/config/config.h
+++ smart_bridge/src/config/config.h
@@ -1,32 +1,37 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
+// LED
+#define L1_PIN 7 // Green diode led
+#define L2_PIN 6 // Red diode led
+#define L3_PIN 5 // Green diode led
 
-//LED
-#define L1_PIN 7
-#define L2_PIN 6
-#define L3_PIN 5
-
-//BUTTON
+// BUTTON
 #define BUTTON_PIN 4
 
-//Termometer
+// Termometer
 #define TMP_PIN A0
-#define TMP_READ_DELAY 500 //read temperature once per second/2 (500 ms)
+#define TMP_READ_DELAY 500 // read temperature once per second/2 (500 ms)
 
-//PIR
+// PIR
 #define PIR_PIN 2
 
-//SONAR
+// SONAR
 #define SONAR_TRIG_PIN 11
 #define SONAR_ECHO_PIN 12
 
-//SERVO
+// SERVO
 #define SERVO_PIN 9
-
 
 /**
  * TODO: TIMING, time constants for example N4.... etc
-*/
+ */
+#define N1 10      // Time for the gate to open after car presence (in seconds)
+#define MINDIST 10 // Minimum distance for car fully entering (in cm)
+#define N2 10      // Time to consider car fully entered (in seconds)
+#define N3 15      // Time for the washing process (in seconds)
+#define MAXDIST 30 // Maximum distance to consider car leaving (in cm)
+#define N4 10      // Time to consider car fully left (in seconds)
+#define MAXTEMP 50 // Maximum temperature before maintenance required (in Celsius)
 
 #endif

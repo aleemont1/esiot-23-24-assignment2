18d46ae219f8e124b81dbcae5769dd4050a37a44
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-11-25 16:45:42 +0100

    [MAIN] Example of task creation

--- smart_bridge/src/main.cpp
+++ smart_bridge/src/main.cpp
@@ -1,12 +1,28 @@
 #include "Arduino.h"
+#include "kernel/Scheduler.h"
+#include "tasks/CheckInTask.h"
 
+Scheduler *scheduler = new Scheduler();
 
 void setup()
 {
     Serial.begin(9600);
+    scheduler->init(1900);
+    CheckInTask *checkInTask = new CheckInTask();
+    checkInTask->init();
+    checkInTask->setActive(true);
+
+    if (scheduler->addTask(checkInTask))
+    {
+        Serial.println("Task added");
+    }
+    else
+    {
+        Serial.println("Task not added");
+    }
 }
 
 void loop()
 {
-    
+    scheduler->schedule();
 }

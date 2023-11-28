435892aa22d61aaaa5ba2d2e3ac551a036d1682b
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-11-28 19:28:14 +0100

    Refactor CheckOutTask class to include Sonar functionality and update state machine

--- smart_bridge/src/tasks/CheckOutTask.h
+++ smart_bridge/src/tasks/CheckOutTask.h
@@ -1,35 +1,51 @@
 #ifndef __CHECK_OUT_TASK__
 #define __CHECK_OUT_TASK__
 
-#include "kernel/TaskWithState.h"
+#define SONAR_MAX_TIME 10
+
+#include "kernel/DependantTaskWIthState.h"
+#include "BlinkTask.h"
 #include "components/api/Led.h"
+#include "components/api/Sonar.h"
 #include "config/config.h"
 #include <Servo.h>
 
-class CheckOutTask : public TaskWithState
+class CheckOutTask : public DependantTaskWithState
 {
 public:
-    CheckOutTask() : TaskWithState()
+    CheckOutTask(BlinkTask *blinkTask) : DependantTaskWithState()
     {
-        this->L3 = new Led(L3_PIN);
-        this->gate = new Servo();
-        gate->attach(SERVO_PIN);
-        this->init();
-        this->setState(OPENING);
         Serial.println("CheckOutTask created");
+        this->L2 = new Led(L2_PIN);
+        this->L3 = new Led(L3_PIN);
+        this->sonar = new Sonar(SONAR_TRIG_PIN, SONAR_ECHO_PIN, SONAR_MAX_TIME);
+        this->gate = new Servo();
+        this->gate->detach();
+        this->gate->attach(SERVO_PIN);
+        this->blinkTask = blinkTask;
+        this->blinkTask->isCompleted();
+        this->blinkTask->setActive(false);
+        this->L3->switchOn();
+        this->init();
+        this->setState(OPENING_GATE);
     }
     void tick() override;
 
 private:
-    enum states
+    enum state
     {
-        OPENING,
-        MEASURE,
-        CLOSURE
+        OPENING_GATE,
+        CHECKING_DISTANCE,
+        CLOSING_GATE,
+        TURN_OFF_L3
     };
 
+    Led *L2;
     Led *L3;
     Servo *gate;
+    Sonar *sonar;
+    Task *blinkTask;
+    float detectedDistance; // Distance of the car from the sensor
 };
 
-#endif __CHECK_OUT_TASK__
+#endif // __CHECK_OUT_TASK__

8b7b9338af48ee0483c3e3cb1c0e78fd5fd0f8fd
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-11-28 19:29:18 +0100

    Creates the class def.  for check-out task

--- smart_bridge/src/tasks/CheckOutTask.cpp
+++ smart_bridge/src/tasks/CheckOutTask.cpp
@@ -17,17 +17,48 @@ Checkout:
 
 void CheckOutTask::tick()
 {
-    Serial.println("CheckOutTask::started");
-    switch (this->getState())
+    if (this->getDependency(0)->isCompleted())
     {
-    case OPENING:
-        gate->write(90);
-        Serial.println("CheckOutTask::Opened the gate");
-        this->setState(MEASURE);
-        break;
-    case MEASURE:
+        switch (this->getState())
+        {
+            Serial.println("CheckOutTask::started");
+        case OPENING_GATE:
+            Serial.println("CheckOutTask::Opening the gate");
+            gate->write(90);
+            delay(2000); // Simulates the time needed to open the gate
+            Serial.println("CheckOutTask::Opened the gate");
+            this->setState(CHECKING_DISTANCE);
+            break;
 
-    default:
-        break;
+        case CHECKING_DISTANCE:
+            Serial.println("CheckOutTask::Measuring car distance");
+            this->detectedDistance = sonar->detectDistance();
+            Serial.println("CheckOutTask::The distance detected is: " + String(this->detectedDistance));
+            if (this->detectedDistance > MAXDIST && this->elapsedTime() >= (N4_FOR_DIST * 1000))
+            {
+                Serial.println("CheckOutTask::The car is far enough");
+                this->resetTime();
+                this->setState(CLOSING_GATE);
+            }
+            break;
+
+        case CLOSING_GATE:
+            Serial.println("CheckOutTask::Closing the gate");
+            gate->write(0);
+            delay(2000); // Simulates the time needed to close the gate
+            Serial.println("CheckOutTask::Closed the gate");
+            this->setState(TURN_OFF_L3);
+            break;
+
+        case TURN_OFF_L3:
+            Serial.println("CheckOutTask::Turning off L3");
+            L3->switchOff();
+            this->setCompleted();
+            break;
+
+        default:
+            Serial.println("CheckOutTask::Invalid state");
+            break;
+        }
     }
 }

986f95f69e6062e95bbf7acd0c3a5d4331e8437e
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-11-28 23:50:02 +0100

    Add CountDown task

--- smart_bridge/src/main.cpp
+++ smart_bridge/src/main.cpp
@@ -7,6 +7,7 @@
 #include "tasks/WaitingTask.h"
 #include "tasks/WashingTask.h"
 #include "tasks/CheckOutTask.h"
+#include "tasks/CountDown.h"
 
 Scheduler scheduler;
 
@@ -14,26 +15,28 @@ void setup()
 {
     Serial.begin(9600);
     scheduler.init(50);
-    BlinkTask *blinkTaskTransit = new BlinkTask(L2_PIN);
-    BlinkTask *blinkTaskForWashing = new BlinkTask(L2_PIN);
+    BlinkTask *blinkTask = new BlinkTask(L2_PIN);
     // TODO: BlinkTask *blinkTaskForCheckOutTransit = new BlinkTask(L2_PIN);
     // SleepingTask *sleepingTask = new SleepingTask();
 
-    WaitingTask *waitingTask = new WaitingTask();
+    // WaitingTask *waitingTask = new WaitingTask();
     CheckInTask *checkInTask = new CheckInTask();
-    TransitTask *transitTask = new TransitTask(blinkTaskTransit);
-    WashingTask *washingTask = new WashingTask(blinkTaskForWashing);
+    TransitTask *transitTask = new TransitTask(blinkTask);
+    WashingTask *washingTask = new WashingTask(blinkTask, new CountDown(N3));
+    CountDown *countDown = new CountDown(N3);
+    
     // TODO: CheckOutTask *checkOutTask = new CheckOutTask(blinkTaskForCheckOutTransit);
 
-    scheduler.addTask(waitingTask);
-    // checkInTask->addDependecy(waitingTask)
+    countDown->init(1000);
+    scheduler.addTask(countDown);
+    // scheduler.addTask(waitingTask);
+    // checkInTask->addDependency(waitingTask);
     transitTask->addDependency(checkInTask);
     scheduler.addTask(checkInTask);
     scheduler.addTask(transitTask);
-    scheduler.addTask(blinkTaskTransit);
+    scheduler.addTask(blinkTask);
     washingTask->addDependency(transitTask);
     scheduler.addTask(washingTask);
-    scheduler.addTask(blinkTaskForWashing);
     // TODO: checkOutTask->addDependency(washingTask);
     // TODO: scheduler.addTask(checkOutTask);
     // TODO: scheduler.addTask(blinkTaskForCheckOutTransit);

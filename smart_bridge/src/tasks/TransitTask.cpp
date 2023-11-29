52efe18db6258b5f162be35c8601c3233a468627
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-11-29 18:57:24 +0100

    [TRANSIT] Fix dependency test

--- smart_bridge/src/tasks/TransitTask.cpp
+++ smart_bridge/src/tasks/TransitTask.cpp
@@ -2,73 +2,75 @@
 
 void TransitTask::tick()
 {
-    if (this->getDependency(0)->isCompleted())
+    if (this->getDependency(0) != nullptr)
     {
-        switch (this->getState())
+        if (this->getDependency(0)->isCompleted())
         {
-        case READING_DISTANCE:
-            this->distance = sonar->detectDistance();
-#ifdef __LOG
-            Serial.println("TransitTask::Distance: " + String(this->distance));
-#endif
-            if (this->distance < MINDIST)
+            switch (this->getState())
             {
-                this->resetTime();
-                this->setState(CHECKING_DISTANCE);
+            case READING_DISTANCE:
+                this->L2->switchOn();
+                this->distance = sonar->detectDistance();
 #ifdef __LOG
-                Serial.println("TransitTask::Checking distance");
+                Serial.println("TransitTask::Distance: " + String(this->distance));
 #endif
-            }
-            break;
+                if (this->distance < MINDIST)
+                {
+                    this->resetTime();
+                    this->setState(CHECKING_DISTANCE);
+#ifdef __LOG
+                    Serial.println("TransitTask::Checking distance");
+#endif
+                }
+                break;
 
-        case CHECKING_DISTANCE:
-            if (!this->blinkTask->isActive())
-            {
-                this->blinkTask->resetBlink();
-                this->blinkTask->setActive(true);
-            }
-            this->distance = sonar->detectDistance();
+            case CHECKING_DISTANCE:
+                if (!this->blinkTask->isActive())
+                {
+                    this->blinkTask->setActive(true);
+                }
+                this->distance = sonar->detectDistance();
 #ifdef __LOG
-            Serial.println("TransitTask::Distance: " + String(this->distance));
+                Serial.println("TransitTask::Distance: " + String(this->distance));
 #endif
-            if (this->distance < MINDIST)
-            {
+                if (this->distance < MINDIST)
+                {
 #ifdef __DEBUG
-                Serial.println("TransitTask::Elapsed: " + String(this->elapsedTime()));
+                    Serial.println("TransitTask::Elapsed: " + String(this->elapsedTime()));
 #endif
-                if (this->elapsedTime() >= (N2 * 1000))
+                    if (this->elapsedTime() >= (N2 * 1000))
+                    {
+#ifdef __LOG
+                        Serial.println("TransitTask::Distance: " + String(this->distance));
+                        Serial.println("TransitTask::Car entered");
+                        Serial.println("TransitTask::Closing gate");
+#endif
+                        this->gate->write(0);
+                        if (this->blinkTask->isActive())
+                        {
+                            this->blinkTask->setActive(false);
+                        }
+                        this->L2->switchOn();
+                        this->setCompleted();
+                    }
+                }
+                else
                 {
 #ifdef __LOG
-                    Serial.println("TransitTask::Distance: " + String(this->distance));
-                    Serial.println("TransitTask::Car entered");
-                    Serial.println("TransitTask::Closing gate");
+                    Serial.println("TransitTask::Reading distance");
 #endif
-                    gate->write(0);
                     if (this->blinkTask->isActive())
                     {
-                        this->blinkTask->resetBlink();
+                        this->L2->switchOff();
                         this->blinkTask->setActive(false);
                     }
-                    this->L2->switchOn();
-                    this->setCompleted();
+                    this->setState(READING_DISTANCE);
                 }
+                break;
+            default:
+                Serial.println("TransitTask::Invalid state");
+                break;
             }
-            else
-            {
-#ifdef __LOG
-                Serial.println("TransitTask::Reading distance");
-#endif
-                if (this->blinkTask->isActive())
-                {
-                    this->blinkTask->resetBlink();
-                    this->blinkTask->setActive(false);
-                }
-                this->setState(READING_DISTANCE);
-            }
-            break;
-        default:
-            Serial.println("TransitTask::Invalid state");
-            break;
         }
     }
 }

baf21e752fae1f9639de1662b93991569c5084ac
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-11-30 19:47:10 +0100

    Add countdown functionality to CountDown class def

--- smart_bridge/src/tasks/CountDown.h
+++ smart_bridge/src/tasks/CountDown.h
@@ -3,26 +3,113 @@
 
 #include "kernel/Task.h"
 
+/**
+ * @brief The default value for the count down task.
+ *
+ */
+#define DEFAULT_COUNTDOWN 15
+
+/**
+ * @brief A countdown timer task class.
+ *
+ * This class provides a countdown timer that can be started, paused, resumed, and stopped.
+ * The countdown value can be increased, decreased, and reset.
+ * The current countdown value and whether the countdown is active can be queried.
+ */
 class CountDown : public Task
 {
 public:
-    CountDown(int countDown) : Task()
-    {
-        this->countDown = countDown;
-        this->init(1000);       // Periodic task, executed every 1000ms
-        this->setActive(false); /** NOTE: Might be removed, this stops the countdown by default.
-                                 setActive(true) to start the countdown in your task. **/
-    }
+    /**
+     * @brief Construct a new CountDown object
+     *
+     * @param countDown The initial countdown value.
+     */
+    CountDown(int countDown);
 
+    /**
+     * @brief Tick function that is called periodically.
+     */
     void tick() override;
 
-    int getCountDown()
-    {
-        return countDown;
-    }
+    /**
+     * @brief Get the current countdown value.
+     *
+     * @return int The current countdown value.
+     */
+    int getCountDown();
+
+    /**
+     * @brief Reset the countdown value.
+     *
+     * @param newCountDown The new countdown value.
+     */
+    void resetCountDown(int newCountDown);
+
+    /**
+     * @brief Decrease the countdown value.
+     *
+     * @param decrement The amount to decrease the countdown value by.
+     */
+    void decreaseCountDown(int decrement);
+
+    /**
+     * @brief Increase the countdown value.
+     *
+     * @param increment The amount to increase the countdown value by.
+     */
+    void increaseCountDown(int increment);
+
+    /**
+     * @brief Pause the countdown task by saving its value.
+     *
+     */
+    void pauseCountDown();
+
+    /**
+     * @brief Start the countdown.
+     */
+    void startCountDown();
+
+    /**
+     * @brief Print the countdown value to the serial monitor.
+     *
+     */
+    void printCountDown();
+
+    /**
+     * @brief Ends the countdown task.
+     *
+     */
+    void endsCountDown();
+
+    /**
+     * @brief Prints a message to the serial monitor when the countdown ends.
+     *
+     */
+    void printsEndsCountdown();
+
+    /**
+     * @brief Check if the countdown task is active.
+     *
+     * @return true If the countdown task is active, false otherwise.
+     */
+    bool isCountDownActive();
+
+    /**
+     * @brief Stop the countdown task and reset the countdown value.
+     *
+     */
+    void stopCountDown();
+
+    /**
+     * @brief Resume the countdown task if it was paused.
+     *
+     */
+    void resumeCountDown();
 
 private:
-    int countDown;
+    int countDown;            ///< The current countdown value
+    int pausedCountDown = -1; ///< The countdown value when the countdown was paused, or -1 if the countdown was not paused.
 };
 
 #endif // __COUNT_DOWN_TASK__

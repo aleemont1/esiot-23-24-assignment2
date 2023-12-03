d9499474d554b42ba8a6f738f6d8271fc222db76
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-12-03 23:26:05 +0100

    Refactor ExitTransitTask class

--- smart_bridge/src/tasks/ExitTransitTask.cpp
+++ smart_bridge/src/tasks/ExitTransitTask.cpp
@@ -1,5 +1,18 @@
 #include "ExitTransitTask.h"
 
+ExitTransitTask::ExitTransitTask()
+    : DependantTaskWithState(),
+      GATE_OPEN_POSITION(90),
+      GATE_CLOSE_POSITION(0)
+{
+    Serial.println("CheckOutTask created");
+    this->sonar = new Sonar(SONAR_TRIG_PIN, SONAR_ECHO_PIN, SONAR_MAX_TIME);
+    this->L3 = new Led(L3_PIN);
+    this->gate = new ServoImpl(SERVO_PIN);
+    this->init();
+    this->setState(READING_DISTANCE);
+};
+
 void ExitTransitTask::tick()
 {
     if (this->getDependency(0) != nullptr)
@@ -8,47 +21,56 @@ void ExitTransitTask::tick()
         {
             switch (this->getState())
             {
-
-            case TURN_ON_L3:
-                this->L3->switchOn();
-                this->setState(OPENS_GATE);
-                break;
-
-            case OPENS_GATE:
-                this->gate->write(GATE_OPEN_POSITION);
-                this->setState(READING_DISTANCE);
-                break;
-
             case READING_DISTANCE:
-                this->distance = sonar->detectDistance();
-                Serial.println("ExitTransitTask::Distance: " + String(this->distance));
-                this->setState(CHECKING_DISTANCE);
+                handleReadingDistance();
                 break;
 
             case CHECKING_DISTANCE:
-                if (this->distance > MAXDIST)
-                {
-                    if (timeInExit == 0)
-                    {
-                        timeInExit = millis();
-                    }
-                    else if (millis() - timeInExit >= N4_FOR_DIST * 1000)
-                    {
-                        this->setState(CLOSES_GATE);
-                    }
-                }
+                handleCheckingDistance();
                 break;
 
             case CLOSES_GATE:
-                this->gate->write(GATE_CLOSE_POSITION);
-                this->setState(SWITCH_OFF_L3);
+                handleClosesGate();
                 break;
 
             case SWITCH_OFF_L3:
-                this->L3->switchOff();
-                this->setCompleted();
+                handleSwitchOffL3();
                 break;
             }
         }
     }
+}
+
+void ExitTransitTask::handleReadingDistance()
+{
+    this->distance = sonar->detectDistance();
+    Serial.println("ExitTransitTask::Distance: " + String(this->distance));
+    this->setState(CHECKING_DISTANCE);
+}
+
+void ExitTransitTask::handleCheckingDistance()
+{
+    if (this->distance > MAXDIST)
+    {
+        if (timeInExit == 0)
+        {
+            timeInExit = millis();
+        }
+        else if (millis() - timeInExit >= N4_FOR_DIST * 1000)
+        {
+            this->setState(CLOSES_GATE);
+        }
+    }
+}
+
+void ExitTransitTask::handleClosesGate()
+{
+    this->gate->write(GATE_CLOSE_POSITION);
+    this->setState(SWITCH_OFF_L3);
+}
+
+void ExitTransitTask::handleSwitchOffL3()
+{
+    this->L3->switchOff();
+    this->setCompleted();
 }

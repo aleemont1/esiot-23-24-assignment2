f9c64e4771174cb3a390dee4b7ec8a6ca72d5072
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-12-04 00:19:07 +0100

    Refactor WashingTask class

--- smart_bridge/src/tasks/WashingTask.cpp
+++ smart_bridge/src/tasks/WashingTask.cpp
@@ -1,28 +1,23 @@
 #include "WashingTask.h"
 
-/*
-—
-1)
-Sezione del testo: Washing != CriticalTemp
-- the simulated washing process begins and lasts for N3 secs.
-- During this time, the light L2 blinks with a period equal to 0.5 secs.
-- On the USER LCD a countdown is displayed–either using numbers.
-—
-
-Washing:
-- If PREVIOUS_STATE == ERROR:
-    - COUNT_DOWN = SAVED_COUNT_DOWN
-    - TIME_IN_STATE = SAVED_TIME_IN_STATE
-- If TIME_IN_STATE < N3 sec: [COUNT_DOWN != 0]
-    - LED ROSSO L2 blink (T = 0.5s)
-    - Avvia countdown su LCD
-    - Passa a Critical-Temp.
-*/
-
-void WashingTask::printWashingCompletedMessage()
+WashingTask::WashingTask(BlinkTask *blinkTask,
+                         CountDown *countDownTask,
+                         TemperatureTask *temperatureTask)
+    : DependantTaskWithState()
 {
-    // Serial.println("Washing complete, you can leave the area");
-    lcd->write("Washing complete, you can leave the area", 0, 0);
+    Serial.println("WashingTask created");
+    this->L2 = new Led(L2_PIN);
+    this->L3 = new Led(L3_PIN);
+    this->tempSensor = new Temp(TMP_PIN);
+    this->lcd = new LCD(0x27, 16, 2);
+    this->gate = new ServoImpl(SERVO_PIN);
+    this->blinkTask = blinkTask;
+    this->blinkTask->init(500);        // Blink every 500 ms the red led
+    this->blinkTask->setActive(false); // TODO: Check if this is necessary
+    this->countDownTask = countDownTask;
+    this->temperatureTask = temperatureTask;
+    this->init(1000);
+    this->setState(START_WASHING);
 }
 
 void WashingTask::tick()
@@ -32,55 +27,67 @@ void WashingTask::tick()
         switch (this->getState())
         {
         case START_WASHING:
-            // TODO: this->countDownTask->startCountDown(); check if this is necessary
-            // TODO: this->countDownTask->printCountDown(); check if this is necessary
-            this->blinkTask->setActive(true);
-            this->setState(STARTS_COUNTDOWN);
+            handleStartWashing();
             break;
 
         case STARTS_COUNTDOWN:
-            this->countDownTask->tick();
-            this->temperatureTask->tick();
-            if (this->temperatureTask->checkForCriticalTemperature() == true && this->countDownTask->getStatus() == false)
-            {
-                this->setState(ERROR);
-            }
-            if (this->countDownTask->getStatus() == true)
-            {
-                this->setState(ENDS_COUNTDOWN);
-            }
+            handleStartsCountdown();
             break;
 
         case ENDS_COUNTDOWN:
-            this->countDownTask->endsCountDown();
-            this->blinkTask->setActive(false);
-            this->L3->switchOn();
-            this->printWashingCompletedMessage();
-            this->setCompleted();
-            // TODO: this->setState(OPENS_GATE);
+            handleEndsCountdown();
             break;
-
-            // TODO: Check if this is necessary
-            // case OPENS_GATE:
-
-            //     // TODO: opens the gate
-            //     // TODO: checks for the car leaving the area completely (> then MAXDISTANCE for N4 secs)
-            //     // TODO: closes the gate
-            //     // TODO: this->L3->switchOff();
-            //     this->setCompleted();
-            //     break;
 
         case ERROR:
-            this->countDownTask->pauseCountDown();
-            this->temperatureTask->criticalTemperatureReachedMessage();
-            this->temperatureTask->temperatureMaintenanceMessage();
-            this->serialReceiver->simulateReadData(); // TODO: remove, but now simulate receiving "mnt:done"
-            if (serialReceiver->readData() == true)
-            {
-                this->countDownTask->resumeCountDown();
-                this->setState(STARTS_COUNTDOWN);
-            }
+            handleError();
             break;
         }
+    }
+}
+
+void WashingTask::printWashingCompletedMessage()
+{
+    lcd->write("Washing complete, you can leave the area", 0, 0);
+}
+
+void WashingTask::handleStartWashing()
+{
+    this->blinkTask->setActive(true);
+    this->setState(STARTS_COUNTDOWN);
+}
+
+void WashingTask::handleStartsCountdown()
+{
+    this->countDownTask->tick();
+    this->temperatureTask->tick();
+    if (this->temperatureTask->checkForCriticalTemperature() == true && this->countDownTask->getStatus() == false)
+    {
+        this->setState(ERROR);
+    }
+    if (this->countDownTask->getStatus() == true)
+    {
+        this->setState(ENDS_COUNTDOWN);
+    }
+}
+
+void WashingTask::handleEndsCountdown()
+{
+    this->countDownTask->endsCountDown();
+    this->blinkTask->setActive(false);
+    this->L3->switchOn();
+    this->printWashingCompletedMessage();
+    this->setCompleted();
+}
+
+void WashingTask::handleError()
+{
+    this->countDownTask->pauseCountDown();
+    this->temperatureTask->criticalTemperatureReachedMessage();
+    this->temperatureTask->temperatureMaintenanceMessage();
+    this->serialReceiver->simulateReadData(); // TODO: remove, but now simulate receiving "mnt:done"
+    if (serialReceiver->readData() == true)
+    {
+        this->countDownTask->resumeCountDown();
+        this->setState(STARTS_COUNTDOWN);
     }
 }

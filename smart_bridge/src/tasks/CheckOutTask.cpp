77aebcc2ce901adf22652724f6bcfbca84d7db0f
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-12-03 23:26:17 +0100

    Refactor CheckOutTask to improve readability and add comments

--- smart_bridge/src/tasks/CheckOutTask.cpp
+++ smart_bridge/src/tasks/CheckOutTask.cpp
@@ -1,63 +1,46 @@
 #include "CheckOutTask.h"
 
-/*
-—
-4)
-Sezione del testo: Check-out
-    - and the gate GATE is opened to allow the vehicle to move away from the Washing Area.
-    - In this stage, the leaving process can be considered completed when the CAR DISTANCE DETECTOR measures a distance greater than MAXDIST for N4 secs. At that point, the gate GATE is closed, L3 is turned off.
-—
-Checkout:
-- SONAR misura dist
-- Disabilita PIR
-- If dist > MAXDIST and TIME_IN_STATE > N4 sec:
-    - GATE si chiude
-    - Passa a waiting state
-*/
+CheckOutTask::CheckOutTask() : DependantTaskWithState()
+{
+    Serial.println("CheckOutTask created");
+    this->sonar = new Sonar(SONAR_TRIG_PIN, SONAR_ECHO_PIN, SONAR_MAX_TIME);
+    this->L3 = new Led(L3_PIN);
+    this->gate = new ServoImpl(SERVO_PIN);
+    this->init();
+    this->setState(TURN_ON_L3);
+};
 
 void CheckOutTask::tick()
 {
-    if (this->getDependency(0)->isCompleted())
+    if (this->getDependency(0) != nullptr)
     {
-        Serial.println("CheckOutTask::started");
-        switch (this->getState())
+        if (this->getDependency(0)->isCompleted())
         {
-        case OPENING_GATE:
-            Serial.println("CheckOutTask::Opening the gate");
-            gate->write(90);
-            Serial.println("CheckOutTask::Opened the gate");
-            this->setState(CHECKING_DISTANCE);
-            break;
-
-        case CHECKING_DISTANCE:
-            Serial.println("CheckOutTask::Measuring car distance");
-            this->detectedDistance = sonar->detectDistance();
-            Serial.println("CheckOutTask::The distance detected is: " + String(this->detectedDistance));
-            if (this->detectedDistance > MAXDIST && this->elapsedTime() >= (N4_FOR_DIST * 1000))
+            switch (this->getState())
             {
-                Serial.println("CheckOutTask::The car is far enough");
-                this->resetTime();
-                this->setState(CLOSING_GATE);
+
+            case TURN_ON_L3:
+                handleTurnOnL3();
+                break;
+
+            case OPENS_GATE:
+                handleOpensGate();
+                break;
             }
-            break;
-
-        case CLOSING_GATE:
-            Serial.println("CheckOutTask::Closing the gate");
-            gate->write(0);
-            delay(2000); // Simulates the time needed to close the gate
-            Serial.println("CheckOutTask::Closed the gate");
-            this->setState(TURN_OFF_L3);
-            break;
-
-        case TURN_OFF_L3:
-            Serial.println("CheckOutTask::Turning off L3");
-            L3->switchOff();
-            this->setCompleted();
-            break;
-
-        default:
-            Serial.println("CheckOutTask::Invalid state");
-            break;
         }
     }
+}
+
+void CheckOutTask::handleTurnOnL3()
+{
+    this->L3->switchOn();
+    Serial.println("CheckOutTask::L3 turned on");
+    this->setState(OPENS_GATE);
+}
+
+void CheckOutTask::handleOpensGate()
+{
+    this->gate->write(GATE_OPEN_POSITION);
+    Serial.println("CheckOutTask::Gate opened");
+    this->setCompleted();
 }

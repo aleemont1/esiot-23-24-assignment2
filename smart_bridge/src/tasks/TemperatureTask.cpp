ff12f8fb6b35f0d043b3ae3f80148b350febe36c
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-12-04 00:19:17 +0100

    Refactor TemperatureTask class to improve readability and maintainability

--- smart_bridge/src/tasks/TemperatureTask.cpp
+++ smart_bridge/src/tasks/TemperatureTask.cpp
@@ -1,6 +1,9 @@
 #include "TemperatureTask.h"
 
-TemperatureTask::TemperatureTask() : Task()
+TemperatureTask::TemperatureTask()
+    : Task(),
+      voltageConversionFactor(100),
+      voltageOffset(0.5)
 {
     Serial.println("TemperatureTask created");
     this->lcd = new LCD(0x27, 16, 2);
@@ -11,13 +14,13 @@ TemperatureTask::TemperatureTask() : Task()
 float TemperatureTask::readVoltage()
 {
     int reading = analogRead(TMP_PIN);
-    float voltage = reading * 5.0 / 1024.0;
+    float voltage = reading * ADC_VOLTAGE_RANGE / ADC_RESOLUTION;
     return voltage;
 }
 
 float TemperatureTask::convertVoltageToTemperature(float voltage)
 {
-    float temperatureC = (voltage - 0.5) * 100; // Converting from 10 mV per degree with 500 mV offset
+    float temperatureC = (voltage - voltageOffset) * voltageConversionFactor; // Converting from 10 mV per degree with 500 mV offset
     return temperatureC;
 }
 
@@ -36,7 +39,6 @@ void TemperatureTask::setTemperature(int temperature)
 void TemperatureTask::printTemperature()
 {
     int temp = getTemperature();
-    // Serial.println("Temperature: " + String(temp) + "°C");
     lcd->write(("Temperature: " + String(temp) + "°C").c_str(), 0, 0);
 }
 
@@ -80,5 +82,4 @@ void TemperatureTask::tick()
 {
     startMonitoringTemperature();
     printTemperature();
-    // checkForCriticalTemperature();
 }

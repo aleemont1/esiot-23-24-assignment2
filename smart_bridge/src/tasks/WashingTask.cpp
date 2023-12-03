4ca4ea18757986273fa8174ca76b7e48f9d1359c
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-12-03 21:15:48 +0100

    Fix washing task state transitions

--- smart_bridge/src/tasks/WashingTask.cpp
+++ smart_bridge/src/tasks/WashingTask.cpp
@@ -21,7 +21,7 @@ Washing:
 
 void WashingTask::printWashingCompletedMessage()
 {
-    Serial.println("Washing complete, you can leave the area");
+    // Serial.println("Washing complete, you can leave the area");
     lcd->write("Washing complete, you can leave the area", 0, 0);
 }
 
@@ -32,91 +32,55 @@ void WashingTask::tick()
         switch (this->getState())
         {
         case START_WASHING:
-            this->countDownTask->startCountDown();
+            // TODO: this->countDownTask->startCountDown(); check if this is necessary
+            // TODO: this->countDownTask->printCountDown(); check if this is necessary
             this->blinkTask->setActive(true);
-            this->setState(COUNTDOWN);
+            this->setState(STARTS_COUNTDOWN);
             break;
 
-            // TODO: fix the if statement currently not working
-        case COUNTDOWN:
-            if (this->countDownTask->isCompleted())
+        case STARTS_COUNTDOWN:
+            this->countDownTask->tick();
+            this->temperatureTask->tick();
+            if (this->temperatureTask->checkForCriticalTemperature() == true && this->countDownTask->getStatus() == false)
             {
-                this->countDownTask->endsCountDown();
-                this->blinkTask->setActive(false);
-                this->L3->switchOn();
-                this->printWashingCompletedMessage();
-                // TODO: opens the gate
-                // TODO: checks for the car leaving the area completely (> then MAXDISTANCE for N4 secs)
-                // TODO: closes the gate
-                // TODO: this->L3->switchOff();
-                this->setCompleted();
+                this->setState(ERROR);
             }
-            else
+            if (this->countDownTask->getStatus() == true)
             {
-                this->countDownTask->tick();
-                this->temperatureTask->tick();
+                this->setState(ENDS_COUNTDOWN);
             }
             break;
 
-        case ERROR:
+        case ENDS_COUNTDOWN:
+            this->countDownTask->endsCountDown();
+            this->blinkTask->setActive(false);
+            this->L3->switchOn();
+            this->printWashingCompletedMessage();
+            this->setCompleted();
+            // TODO: this->setState(OPENS_GATE);
             break;
 
-            // if (this->getPreviousState() == ERROR)
-            // {
-            //     this->setState(ERROR_RECOVERY);
-            // }
-            // Serial.println("WashingTask:: The washing process is started");
-            // this->resetTime(); // Reset the time spent in the current state
-            // this->countDownTask->startCountDown();
-            // Serial.println("WashingTask::The countdown is started");
-            // this->blinkTask->setActive(true);
-            // Serial.println("WashingTask::L2 blinking started");
-            // this->setState(COUNTDOWN);
-            // break;
+            // TODO: Check if this is necessary
+            // case OPENS_GATE:
 
-            // // TODO: To this purpose, during the washing process the temperature is monitored
-            //             // by means of the TEMP sensor, reporting  the ongoing value on the PC Console Dashboard.
-            //             Serial.println("WashingTask::Temperature monitored");
-            //             Serial.println("WashingTask::Temperature printed on PC Console Dashboard" + String(tempSensor->getTemp()));
-
-            //             if ((tempSensor->getTemp() > MAXTEMP) && (this->elapsedTime() > (N4_FOR_TEMP * 1000)))
-            //             {
-            //                 // TODO: Stop the washing process and the countdown
-            //                 Serial.println("WashingTask::Temperature is critical");
-            //                 // TODO: then the washing process  is suspended and a message Maintenance required is displayed on the PC Console Dashboard
-            //                 // TODO: lcd->print("Detected a Problem - Please Wait");
-            //                 Serial.println("WashingTask::Detected a Problem - Please Wait");
-            //                 savedCountDown = ((countDown * 1000) - elapsedTime()); // Save the countdown before the error
-            //                 this->setState(ERROR);
-            //             }
-            //             if (this->countDownTask->isCompleted())
-            //             {
-            //                 this->setCompleted();
-            //             }
-            //             break;
-
-            // case ERROR_RECOVERY:
-            //     Serial.println("WashingTask::Error recovery started");
-            //     savedCountDown = countDown; // saves the countdown before the error
-            //     this->countDownTask->setActive(false);
-            //     Serial.println("WashingTask::The countdown is stopped");
-            //     Serial.println("WashingTask::The countdown is restored" + String(savedCountDown));
-            //     this->setState(COUNTDOWN); // Change the state back to COUNTDOWN
+            //     // TODO: opens the gate
+            //     // TODO: checks for the car leaving the area completely (> then MAXDISTANCE for N4 secs)
+            //     // TODO: closes the gate
+            //     // TODO: this->L3->switchOff();
+            //     this->setCompleted();
             //     break;
 
-            // case ERROR:
-            //     Serial.println("WashingTask::Error detected");
-            //     Serial.println("WashingTask::The washing process is suspended");
-            //     Serial.println("Simulates the time to receive and press the buttons on the UI");
-            //     Serial.println("WashingTask::OK message received");
-            //     this->setState(START_WASHING);
-            // TODO: Missing the ok message from the serial
-            // if (Serial.read() == "Messaggio di OK da seriale")
-            // {
-            //     Serial.println("WashingTask::OK message received");
-            //     delay(500);
-            //     this->setState(START_WASHING);
-            // }
+        case ERROR:
+            this->countDownTask->pauseCountDown();
+            this->temperatureTask->criticalTemperatureReachedMessage();
+            this->temperatureTask->temperatureMaintenanceMessage();
+            this->serialReceiver->simulateReadData(); // TODO: remove, but now simulate receiving "mnt:done"
+            if (serialReceiver->readData() == true)
+            {
+                this->countDownTask->resumeCountDown();
+                this->setState(STARTS_COUNTDOWN);
+            }
+            break;
         }
     }
 }

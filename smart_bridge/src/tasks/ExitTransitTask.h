090f0bd56b0320cb4b1bf2d1e67c728bd729ab6f
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-12-03 23:25:59 +0100

    Refactor ExitTransitTask class documentation

--- smart_bridge/src/tasks/ExitTransitTask.h
+++ smart_bridge/src/tasks/ExitTransitTask.h
@@ -8,7 +8,7 @@
 #include "components/api/ServoImpl.h"
 
 /**
- * @brief Task for managing the exit transit process.
+ * @brief Manages the process of a car exiting the transit area.
  *
  * This task is responsible for managing the process of a car exiting the transit area.
  * It reads the distance from the sonar sensor, checks if the distance is within a certain range,
@@ -19,65 +19,68 @@
 class ExitTransitTask : public DependantTaskWithState
 {
 public:
-    ExitTransitTask() : DependantTaskWithState()
-    {
-        Serial.println("ExitTransitTask created");
-        this->sonar = new Sonar(SONAR_TRIG_PIN, SONAR_ECHO_PIN, SONAR_MAX_TIME);
-        this->L3 = new Led(L3_PIN);
-        this->gate = new ServoImpl(SERVO_PIN);
-        this->init();
-        this->setState(READING_DISTANCE);
-    };
+    /**
+     * @brief Constructs a new ExitTransitTask.
+     *
+     * Initializes the sonar, L3 LED, and gate, and sets the initial state to READING_DISTANCE.
+     */
+    ExitTransitTask();
 
     /**
-     * @brief Tick function that is called periodically.
+     * @brief Called periodically to update the task's state.
+     *
+     * This function is overridden from DependantTaskWithState.
      */
     void tick() override;
 
+    /**
+     * @brief Handles the reading of the distance from the sonar sensor.
+     *
+     * This function is called when the task's state is READING_DISTANCE.
+     */
+    void handleReadingDistance();
+
+    /**
+     * @brief Handles the checking of the distance from the sonar sensor.
+     *
+     * This function is called when the task's state is CHECKING_DISTANCE.
+     */
+    void handleCheckingDistance();
+
+    /**
+     * @brief Handles the closing of the gate.
+     *
+     * This function is called when the task's state is CLOSES_GATE.
+     */
+    void handleClosesGate();
+
+    /**
+     * @brief Handles the switching off of the L3 LED.
+     *
+     * This function is called when the task's state is SWITCH_OFF_L3.
+     */
+    void handleSwitchOffL3();
+
 private:
+    /**
+     * @brief Enum for the possible states of the task.
+     *
+     */
     enum state
     {
-        /**
-         * @brief The task is currently turning on the L3 LED.
-         *
-         */
-        TURN_ON_L3,
-        /**
-         * @brief The task is currently opening the gate.
-         *
-         */
-        OPENS_GATE,
-        /**
-         * @brief The task is currently reading the distance from the sonar sensor.
-         *
-         */
-        READING_DISTANCE,
-        /**
-         * @brief The task is checking if the distance is within a certain range.
-         *
-         */
-        CHECKING_DISTANCE,
-        /**
-         * @brief The task is currently closing the gate.
-         *
-         */
-        CLOSES_GATE,
-
-        /**
-         * @brief The task is currently switching off the L3 LED.
-         *
-         */
-        SWITCH_OFF_L3
-
+        READING_DISTANCE,  ///< The task is currently reading the distance from the sonar sensor.
+        CHECKING_DISTANCE, ///< The task is checking if the distance is within a certain range.
+        CLOSES_GATE,       ///< The task is currently closing the gate.
+        SWITCH_OFF_L3      ///< The task is currently switching off the L3 LED.
     };
 
-    Led *L3;                           // Pointer to the L3 LED object
-    Sonar *sonar;                      // Pointer to the sonar object
-    ServoImpl *gate;                   // Pointer to the ServoImpl object that controls the gate.
-    float distance;                    // The current distance read from the sonar sensor.
-    unsigned long timeInExit = 0;      // The time in milliseconds when the car entered the exit area.
-    const int GATE_OPEN_POSITION = 90; // The position of the gate when it is open.
-    const int GATE_CLOSE_POSITION = 0; // The position of the gate when it is closed.
+    Led *L3;                       ///< Pointer to the L3 LED object.
+    Sonar *sonar;                  ///< Pointer to the sonar object.
+    ServoImpl *gate;               ///< Pointer to the ServoImpl object that controls the gate.
+    float distance;                ///< The current distance read from the sonar sensor.
+    unsigned long timeInExit;      ///< The time in milliseconds when the car entered the exit area.
+    const int GATE_OPEN_POSITION;  ///< The position of the gate when it is open.
+    const int GATE_CLOSE_POSITION; ///< The position of the gate when it is closed.
 };
 
-#endif __EXIT_TRANSIT_TASK__
+#endif // __EXIT_TRANSIT_TASK__

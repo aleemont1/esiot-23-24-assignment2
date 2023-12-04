782dbea10fc4312dd179df052530fdd6a2736e8b
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-12-04 16:32:18 +0100

    [CLICK] Add LOG options

--- smart_bridge/src/tasks/WaitForClickTask.cpp
+++ smart_bridge/src/tasks/WaitForClickTask.cpp
@@ -4,12 +4,17 @@ void WaitForClickTask::tick()
 {
     if (this->getDependency(0) != nullptr)
     {
-        //Serial.println("WaitForClickTask::getDependency(0) != nullptr");
         if (this->getDependency(0)->isCompleted())
         {
+#ifdef __LOG
+            Serial.println("WaitForClickTask::Waiting for START click");
+#endif
+            this->start->sync();
             if (this->start->isPressed())
             {
+#ifdef __LOG
                 Serial.println("WaitForClickTask::START pressed");
+#endif
                 this->setCompleted();
             }
         }

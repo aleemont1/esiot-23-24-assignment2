4e5818a5879941a4094955de731e44e30e2e295a
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2023-12-05 21:54:05 +0100

    Update gate open position and add LCD display

--- smart_bridge/src/tasks/ExitTransitTask.cpp
+++ smart_bridge/src/tasks/ExitTransitTask.cpp
@@ -2,60 +2,60 @@
 
 ExitTransitTask::ExitTransitTask()
     : DependantTaskWithState(),
-      GATE_OPEN_POSITION(90),
+      GATE_OPEN_POSITION(180),
       GATE_CLOSE_POSITION(0)
 {
     Serial.println("ExitTransitTask created");
     this->sonar = new Sonar(SONAR_TRIG_PIN, SONAR_ECHO_PIN, SONAR_MAX_TIME);
     this->L3 = new Led(L3_PIN);
     this->gate = new ServoImpl(SERVO_PIN);
+    this->lcd = new LCD(0x27, 16, 2);
     this->init();
     this->setState(READING_DISTANCE);
 };
 
 void ExitTransitTask::tick()
 {
-    if (this->getDependency(0) != nullptr)
+
+    switch (this->getState())
     {
-        if (this->getDependency(0)->isCompleted())
-        {
-            switch (this->getState())
-            {
-            case READING_DISTANCE:
-                handleReadingDistance();
-                break;
+    case READING_DISTANCE:
+        handleReadingDistance();
+        break;
 
-            case CHECKING_DISTANCE:
-                handleCheckingDistance();
-                break;
+    case CHECKING_DISTANCE:
+        handleCheckingDistance();
+        break;
 
-            case CLOSES_GATE:
-                handleClosesGate();
-                break;
+    case CLOSES_GATE:
+        handleClosesGate();
+        break;
 
-            case SWITCH_OFF_L3:
-                handleSwitchOffL3();
-                break;
-            }
-        }
+    case SWITCH_OFF_L3:
+        handleSwitchOffL3();
+        break;
     }
-    else
-        Serial.println("ExitTransitTask::tick()::getDependency(0) is nullptr");
 }
 
 void ExitTransitTask::handleReadingDistance()
 {
-    this->distance = sonar->detectDistance();
-    Serial.println("ExitTransitTask::Distance: " + String(this->distance));
+    this->lcd->write("Exit area", 0, 0);
+    this->lcd->write("Proceed slowly!", 0, 1);
+    this->L3->switchOn();
     this->setState(CHECKING_DISTANCE);
 }
 
 void ExitTransitTask::handleCheckingDistance()
 {
+    this->distance = sonar->detectDistance();
+    Serial.println("ExitTransitTask::Distance: " + String(this->distance));
     if (this->distance > MAXDIST)
     {
-        if (this->elapsedTime() >= N4_FOR_DIST * 1000)
+        this->distance = sonar->detectDistance();
+        Serial.println("ExitTransitTask::Distance: " + String(this->distance));
+        if (this->elapsedTime() >= (N4_FOR_DIST * 1000))
         {
+            Serial.println("ExitTransitTask::Car left");
             this->setState(CLOSES_GATE);
         }
     }
